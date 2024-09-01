c091728a8849856e4301c595e4ac49f0342e50a0
amaiNikaido <149743846+amaiNikaido@users.noreply.github.com>
2024-08-31 22:20:08 -0300

    organiza as pastas
    eu refiz o código, ta mais bonito

--- Script/ovos.gd
+++ Script/ovos.gd
@@ -9,3 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	Globais.ovos += 1
+	queue_free()
